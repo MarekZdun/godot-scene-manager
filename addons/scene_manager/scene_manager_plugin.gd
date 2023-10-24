7a57f5d3b0108c3b4b5959f405607ff0da9d3db8
Marek <marek.sebastian.zdun@gmail.com>
2023-10-24 11:45:47 +0200

    fix function names

--- addons/scene_manager/scene_manager_plugin.gd
+++ addons/scene_manager/scene_manager_plugin.gd
@@ -5,9 +5,9 @@ const SCENE_MANAGER_FILEPATH = "res://addons/scene_manager/scene_manager.tscn"
 const SCENE_MANAGER_AUTLOAD_NAME = "SceneManager"
 
 
-func enable_plugin():
+func _enable_plugin():
 	add_autoload_singleton(SCENE_MANAGER_AUTLOAD_NAME, SCENE_MANAGER_FILEPATH)
 
 
-func disable_plugin():
+func _disable_plugin():
 	remove_autoload_singleton(SCENE_MANAGER_AUTLOAD_NAME)
